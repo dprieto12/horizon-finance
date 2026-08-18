177c53c04cd9b226693fda456e637ca3569a03d1
David Prieto <davidjoseph.prieto@calbaptist.edu>
2026-08-18 07:47:29 -0700

    Implemented the account dashboard with navigation buttons to transactions, analytics, and account settings.

--- src/main/java/controllers/DashboardController.java
+++ src/main/java/controllers/DashboardController.java
@@ -1,4 +1,47 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+
+import utils.ApplicationState;
+import utils.SceneManager;
+
+import java.io.IOException;
+
+
 public class DashboardController {
+    @FXML
+    private Label welcomeBackLabel;
+
+    @FXML
+    private Label accountBalanceLabel;
+
+    @FXML
+    public void initialize() {
+        updateWelcomeLabel();
+        updateAccountBalanceLabel();
+    }
+
+    private void updateWelcomeLabel() {
+        String customWelcome = "Welcome back, " + ApplicationState.getCurrentAccount().getFirstName() + "!";
+        welcomeBackLabel.setText(customWelcome);
+    }
+
+    private void updateAccountBalanceLabel() {
+        String customBalance = "Account Balance: $" + ApplicationState.getCurrentAccount().getBalance();
+        accountBalanceLabel.setText(customBalance);
+    }
+
+    public void viewTransactions() throws IOException {
+        SceneManager.switchScene("/fxml/transactionOptions.fxml");
+    }
+
+    public void viewAnalytics() throws IOException {
+        SceneManager.switchScene("/fxml/viewAnalytics.fxml");
+    }
+
+    public void viewAccount() throws IOException {
+        SceneManager.switchScene("/fxml/accountSettings");
+    }
 }
